e83508fa92009e7a5406fdcaeaae4dd2008deca5
Alice Kile <buoyantair@protonmail.com>
2026-02-20 20:40:53 +0100

    setup prisma orm

---
+++ src/db.ts
@@ -0,0 +1,42 @@
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client.ts";
+import { fileURLToPath } from "node:url";
+import process from "node:process";
+import "dotenv/config";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter });
+export const db = prisma;
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+    },
+  });
+  console.log("Created user:", user);
+  // Fetch all users with their posts
+  const allUsers = await prisma.user.findMany({
+    include: {
+      bookmarks: true,
+    },
+  });
+  console.log("All users:", JSON.stringify(allUsers, null, 2));
+}
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  main()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world')
+import { router, publicProcedure } from "./trpc.ts";
+import { db } from './db.ts'
+
+
+const appRouter = router({
+  userList: publicProcedure.query(async () => {
+    const users = await db.user.findMany()
+    return users
+  })
+})
+
+export type AppRouter = typeof appRouter
