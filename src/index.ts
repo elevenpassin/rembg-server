005d97db62e855e2ecdf5863a2ea21a2a51fbc5c
Alice Kile <buoyantair@protonmail.com>
2026-02-21 12:05:12 +0100

    setup user By Id

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
+import z from "zod";
 import { db } from "./db.ts";
 import { publicProcedure, router } from "./trpc.ts";
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
 
 const appRouter = router({
 	userList: publicProcedure.query(async () => {
 		const users = await db.user.findMany();
 		return users;
 	}),
+	userById: publicProcedure.input(z.int()).query(async (opts) => {
+		const { input } = opts;
+
+		const user = await db.user.findUnique({
+			where: {
+				id: input,
+			},
+		});
+
+		return user;
+	}),
 });
 
 export type AppRouter = typeof appRouter;
+
+const server = createHTTPServer({
+	router: appRouter,
+});
+server.listen(3000);
